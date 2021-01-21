b34b4223540d6ac00edeff73c44d1e22f1204c8f
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2021-01-21 15:11:32 +0700

    Update tugas1.php

--- tugas1.php
+++ tugas1.php
@@ -1,41 +1,9 @@
-<?php
-for ($i = 7; $i > 0; $i--) {
-    echo $i; // 7 6 5 4 3 2 1
-   
-}
-echo "<br>";
-for ($i = 6; $i > 0; $i--) {
-echo $i; //  6 5 4 3 2 1
-   
-}echo "<br>";
-for ($i = 5; $i >0; $i--) {
-echo $i; //  5 4 3 2 1
-   
-}echo "<br>";
-for ($i = 4; $i >0; $i--) {
-echo $i; // 4 3 2 1
-  
-} echo "<br>";
-for ($i = 3; $i >0; $i--) {
-echo $i; // 3 2 1
-   
-}echo "<br>";
-for ($i = 2; $i >0; $i--) {
-echo $i; //  2 1
-   
-}echo "<br>";
-for ($i = 1; $i >0; $i--) {
-echo $i; //   1
-   
-}echo "<br>";
-?>
-<?php
-   echo "Saya sedang belajar PHP di duniailkom.com";
-   echo "<br>";
-   echo "Hello World...";
 
+<?php
+   echo "Aku akhirnya selesai".'<br>';
+   echo "alifudinniko".'<br>';
+   echo "M3118007".'<br>';
    echo '<br>';
-
 
 for ($i = 1; $i <= 7; $i++) {
 	for ($j = 8; $i <= $j ; $j--) {
