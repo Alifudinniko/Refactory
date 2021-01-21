84732c9340f868c15ea1a641405efd7c590c5e90
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2021-01-21 13:50:00 +0700

    Create tugas1.php

---
+++ tugas1.php
@@ -0,0 +1,36 @@
+<?php
+for ($i = 7; $i > 0; $i--) {
+    echo $i; // 7 6 5 4 3 2 1
+   
+}
+echo "<br>";
+for ($i = 6; $i > 0; $i--) {
+echo $i; //  6 5 4 3 2 1
+   
+}echo "<br>";
+for ($i = 5; $i >0; $i--) {
+echo $i; //  5 4 3 2 1
+   
+}echo "<br>";
+for ($i = 4; $i >0; $i--) {
+echo $i; // 4 3 2 1
+  
+} echo "<br>";
+for ($i = 3; $i >0; $i--) {
+echo $i; // 3 2 1
+   
+}echo "<br>";
+for ($i = 2; $i >0; $i--) {
+echo $i; //  2 1
+   
+}echo "<br>";
+for ($i = 1; $i >0; $i--) {
+echo $i; //   1
+   
+}echo "<br>";
+?>
+<?php
+   echo "Saya sedang belajar PHP di duniailkom.com";
+   echo "<br>";
+   echo "Hello World...";
+?>
