3a3aedd0afcd5078bef297cc2cd86fd8efe9cceb
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2021-01-22 09:11:43 +0700

    Update Logic2.php

--- 2.Logic2/Logic2.php
+++ 2.Logic2/Logic2.php
@@ -1,20 +1,13 @@
 <?php
-$n=6;
- 
-for($i = 1 ; $i <= ($n+1) ; $i++){
-    for($j = 1 ; $j <= ($n +
-1); $j++)
-     {
-  if(( $i == $j ) || ($j == ($n + 2 - $i) )){ 
-  echo (" # ");
-  }
-  
-   else
-  {
+$n = 6;
 
-   echo "&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp";
-  }
- }
-echo "<br>";
+for ($i = 1; $i <= ($n + 1); $i++) {
+    for ($j = 1; $j <= ($n + 1); $j++) {
+        if (($i == $j) || ($j == ($n + 2 - $i))) {
+            echo (" # ");
+        } else {
+            echo "&nbsp" . "&nbsp" . "&nbsp" . "&nbsp" . "&nbsp";
+        }
+    }
+    echo "<br>";
 }
-?>
