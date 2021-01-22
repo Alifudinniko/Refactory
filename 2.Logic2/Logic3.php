4c8b69e4d8c10d274b2776642f8f4216a40c0256
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2021-01-22 10:13:23 +0700

    buat form

--- 2.Logic2/Logic3.php
+++ 2.Logic2/Logic3.php
@@ -1,29 +1,34 @@
+<form action=”logic3.php” method=”post”>
+	<h1>n : <br>
+		<input type=”text” name=”n”><br>
+		<input type=”submit” value=”Hitung”>
+</form>
 <?php
-$n=6;
-for($i=1;$i<=$n*2+1;$i++){
-echo (" # ");
+
+$n   = $_POST[‘n’];
+
+
+for ($i = 1; $i <= $n * 2 + 1; $i++) {
+	echo (" # ");
 }
 echo "</br>";
-for($i = 1 ; $i <= ($n+1) ; $i++){
-    for($j = 1 ; $j <= ($n + 2); $j++){
-		if($j==1){
+for ($i = 1; $i <= ($n + 1); $i++) {
+	for ($j = 1; $j <= ($n + 2); $j++) {
+		if ($j == 1) {
 			echo ("#");
 		}
-		if($j==($n+2)){
+		if ($j == ($n + 2)) {
 			echo ("#");
 		}
-		if(( $i == $j ) || ($j == ($n + 2 - $i) )){ 
+		if (($i == $j) || ($j == ($n + 2 - $i))) {
 			echo (" # ");
+		} else {
+			echo "&nbsp" . "&nbsp" . "&nbsp" . "&nbsp" . "&nbsp";
 		}
-		 
-		else
-		{
-			echo "&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp";
-		}
- }
-echo "<br>";
+	}
+	echo "<br>";
 }
-for($i=1;$i<=$n*2+1;$i++){
-echo (" # ");
+for ($i = 1; $i <= $n * 2 + 1; $i++) {
+	echo (" # ");
 }
 ?>
---
+++ 2.Logic2/form.php
@@ -0,0 +1,7 @@
+<form action=”logic3.php” method=”post”>
+	<h1>n : <br>
+	<input type=”text” name=”n”><br>
+		
+<input type=”submit” value=”Hitung”>
+
+</form>
